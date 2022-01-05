ac4341c38642ca529bacd7f29e6e6576ba8a8d3f
Souvik Saha Joy <souviksahajoy@gmail.com>
2022-01-06 05:08:22 +0600

    v6

--- dist/librabry.js
+++ dist/librabry.js
@@ -16,23 +16,55 @@ script$1.render = render$1;
 script$1.__file = "src/components/dtInput.vue";
 
 var script = {
-  name: "dtCard"
+  name: "dtCard",
+  props: {
+    type: String,
+    default: '1'
+  }
 };
 
 const _hoisted_1 = { class: "dt-card" };
 const _hoisted_2 = {
   key: 0,
-  class: "dt-card-title"
+  class: "dt-card__img"
 };
+const _hoisted_3 = {
+  key: 0,
+  class: "dt-card__interactions"
+};
+const _hoisted_4 = {
+  key: 1,
+  class: "dt-card__text"
+};
+const _hoisted_5 = { key: 0 };
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (vue.openBlock(), vue.createElementBlock("div", _hoisted_1, [
-    (_ctx.$slots.title)
-      ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_2, [
-          vue.renderSlot(_ctx.$slots, "title")
-        ]))
-      : vue.createCommentVNode("v-if", true)
-  ]))
+  return (vue.openBlock(), vue.createElementBlock("div", {
+    class: vue.normalizeClass(`dt-card-content type-${this.type}`)
+  }, [
+    vue.createElementVNode("div", _hoisted_1, [
+      (_ctx.$slots.img)
+        ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_2, [
+            vue.renderSlot(_ctx.$slots, "img"),
+            (_ctx.$slots.interactions)
+              ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_3, [
+                  vue.renderSlot(_ctx.$slots, "interactions")
+                ]))
+              : vue.createCommentVNode("v-if", true)
+          ]))
+        : vue.createCommentVNode("v-if", true),
+      (_ctx.$slots.text)
+        ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_4, [
+            vue.renderSlot(_ctx.$slots, "text"),
+            (_ctx.$slots.title)
+              ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_5, [
+                  vue.renderSlot(_ctx.$slots, "title")
+                ]))
+              : vue.createCommentVNode("v-if", true)
+          ]))
+        : vue.createCommentVNode("v-if", true)
+    ])
+  ], 2 /* CLASS */))
 }
 
 script.render = render;
--- dist/librabry.mjs
+++ dist/librabry.mjs
@@ -1,4 +1,4 @@
-import { openBlock, createElementBlock, renderSlot, createCommentVNode } from 'vue';
+import { openBlock, createElementBlock, normalizeClass, createElementVNode, renderSlot, createCommentVNode } from 'vue';
 
 var script$1 = {
   name: 'dtInput'
@@ -14,23 +14,55 @@ script$1.render = render$1;
 script$1.__file = "src/components/dtInput.vue";
 
 var script = {
-  name: "dtCard"
+  name: "dtCard",
+  props: {
+    type: String,
+    default: '1'
+  }
 };
 
 const _hoisted_1 = { class: "dt-card" };
 const _hoisted_2 = {
   key: 0,
-  class: "dt-card-title"
+  class: "dt-card__img"
 };
+const _hoisted_3 = {
+  key: 0,
+  class: "dt-card__interactions"
+};
+const _hoisted_4 = {
+  key: 1,
+  class: "dt-card__text"
+};
+const _hoisted_5 = { key: 0 };
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (openBlock(), createElementBlock("div", _hoisted_1, [
-    (_ctx.$slots.title)
-      ? (openBlock(), createElementBlock("div", _hoisted_2, [
-          renderSlot(_ctx.$slots, "title")
-        ]))
-      : createCommentVNode("v-if", true)
-  ]))
+  return (openBlock(), createElementBlock("div", {
+    class: normalizeClass(`dt-card-content type-${this.type}`)
+  }, [
+    createElementVNode("div", _hoisted_1, [
+      (_ctx.$slots.img)
+        ? (openBlock(), createElementBlock("div", _hoisted_2, [
+            renderSlot(_ctx.$slots, "img"),
+            (_ctx.$slots.interactions)
+              ? (openBlock(), createElementBlock("div", _hoisted_3, [
+                  renderSlot(_ctx.$slots, "interactions")
+                ]))
+              : createCommentVNode("v-if", true)
+          ]))
+        : createCommentVNode("v-if", true),
+      (_ctx.$slots.text)
+        ? (openBlock(), createElementBlock("div", _hoisted_4, [
+            renderSlot(_ctx.$slots, "text"),
+            (_ctx.$slots.title)
+              ? (openBlock(), createElementBlock("div", _hoisted_5, [
+                  renderSlot(_ctx.$slots, "title")
+                ]))
+              : createCommentVNode("v-if", true)
+          ]))
+        : createCommentVNode("v-if", true)
+    ])
+  ], 2 /* CLASS */))
 }
 
 script.render = render;
