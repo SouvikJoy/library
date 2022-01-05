a34b2f01e0e362ca3faee8aacd2f1dfca0856c29
Souvik Saha Joy <souviksahajoy@gmail.com>
2022-01-06 04:49:11 +0600

    v4

--- src/components/dtCard.vue
+++ src/components/dtCard.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="dt-card">
-    <div class="dt-card-title" v-if="$slots.title">
-      <slot name="title"></slot>
+  <div :class="`dt-card-content type-${this.type}`">
+    <div class="dt-card">
+      <div class="dt-card__img" v-if="$slots.img">
+        <slot name="img"></slot>
+        <div class="dt-card__interactions" v-if="$slots.interactions">
+          <slot name="interactions"></slot>
+        </div>
+      </div>
+      <div class="dt-card__text" v-if="$slots.text">
+        <slot name="text"></slot>
+        <div v-if="$slots.title">
+          <slot name="title"></slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "dtCard"
+  name: "dtCard",
+  props: {
+    type: String,
+    default: '1'
+  }
 }
 </script>
 
